0745468916272d587ca63f4178c6737570b27235
Andre Dias <andrepldias@hotmail.com>
2019-07-31 16:26:47 +0100

    add 2.6

---
+++ chapter2/exercise_2.06.rkt
@@ -0,0 +1,33 @@
+#lang sicp
+
+(define zero (lambda (f) (lambda (x) x)))
+
+(define (add-1 n)
+  (lambda (f) (lambda (x) (f ((n f) x)))))
+
+;one
+(add-1 zero)
+(lambda (f) (lambda (x) (f (((lambda (f) (lambda (x) x)) f) x))))
+(lambda (f) (lambda (x) (f ((lambda (x) x) x))))
+(lambda (f) (lambda (x) (f x)))
+
+(define one (lambda (f) (lambda (x) (f x))))
+
+;two
+(add-1 one)
+(lambda (f) (lambda (x) (f (((lambda (f) (lambda (x) (f x))) f) x))))
+(lambda (f) (lambda (x) (f (((lambda (x) (f x))) x))))
+(lambda (f) (lambda (x) (f (f x))))
+
+(define two (lambda (f) (lambda (x) (f (f x)))))
+;three
+(add-1 two)
+(lambda (f) (lambda (x) (f (((lambda (x) (f (f x))))  x))))
+(lambda (f) (lambda (x) (f(f(f x)))))
+
+(define three (lambda (f) (lambda (x) (f(f(f x))))))
+;We start to see a pattern here:
+;Zero : (lambda (f) (lambda (x) x))
+;One  : (lambda (f) (lambda (x) (f x)))
+;Two  : (lambda (f) (lambda (x) (f (f x))))
+;Three: (lambda (f) (lambda (x) (f(f(f x)))))
