43e2bf084e100462032242b69ab8088bc96b02f6
Andre Dias <andrepldias@hotmail.com>
2019-08-01 12:18:27 +0100

    add 2.8

---
+++ chapter2/exercise_2.08.rkt
@@ -0,0 +1,6 @@
+#lang sicp
+(define (sub-interval x y)
+  (make-interval (- (lower-bound x)
+                    (upper-bound y))
+                 (- (upper-bound x)
+                    (lower-bound y))))
