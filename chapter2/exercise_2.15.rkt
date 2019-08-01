cbf76d00e1d6e0e8a400fc41a32b70e27a950938
Andre Dias <andrepldias@hotmail.com>
2019-08-01 17:41:03 +0100

    add 2.15

---
+++ chapter2/exercise_2.15.rkt
@@ -0,0 +1,4 @@
+;It's hard to say that one program is better than the other in a corrected precision system.
+;They should produce the same result as one is a mathematic reduction of another. In this
+;case one could say that par2 gives tighter error bounds and that's because it only uses
+;R1 and R2 in its calculation whereas par1 uses R1 and R2 twice each.
