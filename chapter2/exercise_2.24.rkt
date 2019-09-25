bb7a1331efe7ec1a293105fe194b76310e20fec3
andrediasesp <andrepldias@hotmail.com>
2019-09-25 15:30:01 +0100

    add exercise 2.24

---
+++ chapter2/exercise_2.24.rkt
@@ -0,0 +1,12 @@
+#lang sicp
+
+(define (count-leaves x)
+  (cond ((null? x) 0)
+        ((not (pair? x)) 1)
+        (else (+ (count-leaves (car x))
+                 (count-leaves (cdr x))))))
+
+
+(count-leaves (list 1 (list 2 (list 3 4))))
+
+; 4
