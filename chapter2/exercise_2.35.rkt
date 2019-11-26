baa323c2db390b1dd76f6e28abdc665c891b107d
andrediasesp <andrepldias@hotmail.com>
2019-11-26 12:13:20 +0000

    add exercise 2.35

---
+++ chapter2/exercise_2.35.rkt
@@ -0,0 +1,16 @@
+#lang sicp
+
+;accumulate
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (count-leaves t)
+  (accumulate +
+              0
+              (map (lambda (node)
+                          (if (pair? node)
+                              (count-leaves node)
+                              1))t)))
