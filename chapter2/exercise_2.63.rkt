462a47976fbedd4ff3524df997e1197e1b4ecf19
andrediasesp <andrepldias@hotmail.com>
2019-12-16 15:18:24 +0000

    add 2.63

---
+++ chapter2/exercise_2.63.rkt
@@ -0,0 +1,4 @@
+#lang sicp
+
+; a) Both Procedures produce the same list as result
+; b) The first function is of O(n log n) complexity. The second function is of O(n) complexity.  The second one is slower.
